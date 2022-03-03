0edb0979fb5717d6a22c29118c4d4fa7d3707864
Daniel Beck <daniel@vidmob.com>
2022-03-03 16:16:19 -0500

    sample file watcher

---
+++ example/watch-directory.js
@@ -0,0 +1,11 @@
+var chokidar = require('chokidar');
+
+var dir = "./src/";
+
+var watcher = chokidar.watch(dir, {ignored: /^\./, persistent: true});
+
+watcher
+    .on('add', function(path) {console.log('File', path, 'has been added');})
+    .on('change', function(path) {console.log('File', path, 'has been changed');})
+    .on('unlink', function(path) {console.log('File', path, 'has been removed');})
+    .on('error', function(error) {console.error('Error happened', error);})
